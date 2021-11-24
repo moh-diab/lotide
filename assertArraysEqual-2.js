9f1e40094ae57d8d3473ba4f9b0cc13edfeecb7c
moh-diab <mohdiab@gmail.com>
2021-11-24 17:53:34 -0500

    New

---
+++ assertArraysEqual-2.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    return console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => Assertion Passed
+assertArraysEqual(['1', '2', '3',], [1, 2]) // => Assertion Failed
---
+++ without.js
@@ -0,0 +1,28 @@
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    return console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+//A function that removes specific elements of a given array
+const without = function (source, itemsToRemove) {
+  let result = [];
+  for (let element of itemsToRemove) {
+    result = source.filter(item => item !== element);
+  } return result;
+};
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
