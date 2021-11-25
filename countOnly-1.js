8d6ba575a617d3fd899470cba972e904b574c57d
moh-diab <mohdiab@gmail.com>
2021-11-24 21:41:22 -0500

    countOnly

---
+++ countOnly-1.js
@@ -0,0 +1,38 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = [];
+  for (const item of allItems) {
+    if (itemsToCount[item])
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+  } return results;
+};
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(result1)
