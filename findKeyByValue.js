e95fd25589f5e109910fdd26b21493630aa5d007
moh-diab <mohdiab@gmail.com>
2021-11-25 23:52:23 -0500

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj,val) {
+  const extract = Object.keys(obj);
+  for (let key of extract) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "bala bla "), undefined);
