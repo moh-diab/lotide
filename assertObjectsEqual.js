667cf3b6d3be450238cb34c4fe7af91d46f62664
moh-diab <mohdiab@gmail.com>
2021-12-01 22:23:18 -0500

    Implement assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -41,22 +41,33 @@ const eqObjects = function (obj1, obj2) {
   };
 };
 
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  };
+};
+
 
 // test code
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(cd, dc);
+// assertObjectsEqual(ab, cd);
 
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+
+
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(ab, ba), true);
+// assertEqual(eqObjects(ab, abc), false); // => false
+// assertEqual(eqObjects(cd, dc), true); // => true
+// assertEqual(eqObjects(cd, cd2), false); // => false
